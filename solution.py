cd5b7cc7eb80e9cb5ac2f6f5984692a4c2a6dac8
Kooreis <kooreis.official@gmail.com>
2024-10-14 10:35:40 -0500

    Adds a while loop inside the for loop to check if the characters at the left and right pointers are the same. If they are, it increments the count and moves the pointers.

--- solution.py
+++ solution.py
@@ -1,5 +1,4 @@
-def count_palindromic_substrings(s):
-    count = 0
-    for center in range(2*len(s)-1):
-        left = center // 2
-        right = left + center % 2
+while left >= 0 and right < len(s) and s[left] == s[right]:
+            count += 1
+            left -= 1
+            right += 1
