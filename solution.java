dbe93844b6b0e6515e428049b6cb631b67d86d7b
Kooreis <kooreis.official@gmail.com>
2024-10-14 10:35:40 -0500

    In this update, the program calls the countPalindromicSubstrings method with the user's input as an argument. The result is then printed to the console.

--- solution.java
+++ solution.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a string:");
-        String input = scanner.nextLine();
+        ...
+        System.out.println("Number of palindromic substrings: " + countPalindromicSubstrings(input));
     }
 }
